ca62ee4994933ae66f2f0898261c67aa26372833
Wassim Chegham <maneki.nekko@gmail.com>
2015-11-05 11:27:15 +0100

    clean branche

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,18 +3,10 @@
 import { Component, ViewEncapsulation } from 'angular2/angular2';
 import { RouteConfig, Router, Location } from 'angular2/router';
 
-import { Toolbar } from './components/toolbar/toolbar';
-import { QuestionsStore } from './services/QuestionsStore';
-import { APP_ROUTES } from './routes.config';
-
 @Component({
   selector: 'devfest',
-  template: '<toolbar></toolbar>' ,
-  directives: [ Toolbar ],
-  providers: [ QuestionsStore ],
-  encapsulation: ViewEncapsulation.None
+  template: '<h1>Hello Nantes :)</h1>' 
 })
-@RouteConfig(APP_ROUTES)
 export class Devfest {
   constructor(router: Router, location: Location) {
     router.navigate(['/Home']);
