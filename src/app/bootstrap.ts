d9b241140773eb1f212180069a6b257ea3388e3c
Wassim Chegham <maneki.nekko@gmail.com>
2015-11-05 11:35:35 +0100

    clean branch

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,12 +1,6 @@
 /// <reference path="typings/_custom.d.ts" />
 
-import { provide, bootstrap } from 'angular2/angular2';
-import { QUESTIONS } from './data/questions';
-import { ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy } from 'angular2/router';
+import { bootstrap } from 'angular2/angular2';
 import { Devfest } from './app';
 
-bootstrap(Devfest, [
-	ROUTER_PROVIDERS, 
-	provide(Array, {useValue: QUESTIONS}),
-	provide(LocationStrategy, {useClass: HashLocationStrategy})
-]);
+bootstrap(Devfest, []);
